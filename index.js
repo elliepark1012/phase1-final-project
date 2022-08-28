49fcabc29f466a23156ed38abbbab196816cea35
Ellie Park <elliepark1012@gmail.com>
2022-08-28 10:37:17 -0400

    index.js still has error with hourly and top page cut

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-
-
 let today = new Date();
 
 let year = today.getFullYear();
@@ -22,15 +20,14 @@ let dateString = year + '-' + month  + '-' + day;
       };
       xhr.send();
     };
-    
-    let url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/11693/${dateString}?unitGroup=us&key=276XSQHSHP7GMC4YFTCXB24NG&contentType=json`
-
-    getJSON(url,
+  
+    getJSON(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/11693/${dateString}?unitGroup=us&key=276XSQHSHP7GMC4YFTCXB24NG&contentType=json`,
     function(err, data) {
     if(err !== null) {
     alert('unexpected error' + err);
     } else {
     loadWeather(data);
+    hourly(data)
     }
     });
 
@@ -59,31 +56,16 @@ let dateString = year + '-' + month  + '-' + day;
       
     }
 
-let req = new Request(url, {method:'GET'});
 let container, df;
 
-document.addEventListener('DOMContentLoaded', init);
-
-function init(){
-    container = document.getElementById('container');
-    df = new DocumentFragment();
-    
-    fetch(req)
-    .then((response)=>{
-        if(response.ok){
-            return response.json();
-        }else{
-            throw new Error('BAD HTTP');
-        }
-    })
-    .then((json)=>{
-        json.days.forEach((hours)=>{
+function hourly(data){
+        data.days.forEach((hours)=>{
             let div = document.createElement('div');
             div.classList.add('hour');
             let timestamp = hours.datetime;
             div.id = 'ts_' + timestamp.toString();
-            let temp = parseInt(hours.temperature);
-            div.textContent = temp.toString().concat('\u00B0');
+            let temperature = parseInt(hours.temp);
+            div.textContent = temperature.toString().concat('\u00B0');
             div.title = hours.summary;
           
             let span = document.createElement('span');
@@ -94,37 +76,7 @@ function init(){
             df.appendChild(div);
         });
         container.appendChild(df);
-    })
-    .catch((err)=>{
-        console.log( err.message );
-    })
-  }
-
-
-
-
-    // function hourly(){
-    //     data.days.hours.forEach((hours) => {
-    //       let div = document.createElement('div');
-    //       div.classList.add('hour');
-    //       let timeStamp = hours.datetime;
-    //       div.id = 'ts_' + timeStamp.toString();
-    //       let tempHourly = parseInt(hours.temp);
-    //       div.textContent = tempHourly.toString().concat('\u0000');
-    //       div.title = hours.summary;
-
-    //       let span = document.createElement('span');
-    //       span.textContent = "0:00";
-
-    //       let temp = parseInt
-
-    //       div.appendChild(span);
-    //       df.appendChild(div);
-    //     });
-    //     container.appendChild(df);
-    //   }
-
-
+    }
 
     buttonGoogleMap = document.getElementById("googlemap");
     buttonGoogleMap.addEventListener("click",function(){
